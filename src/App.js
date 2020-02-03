0ce913658d5456fa19c32050bcf470fa1f8748ca
DMDXV <Padfootdiamond@Gmail.com>
2020-02-03 12:59:02 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {increment, decrement, reset} from './actions';
+
+//ReactStrap && FontAwesome
+import { Button, ButtonGroup, Container } from 'reactstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRedo, faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  const counter = useSelector(state => state.counterReducer);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="display-3">Counter: {counter} </h1>
+        <div className="mx-auto">
+          <ButtonGroup className="mx-auto">
+            <Button
+              color="success"
+              size="lg"
+              onClick={() => dispatch(increment(1))}
+            >
+              <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
+            </Button>
+
+            <Button color="info" size="lg" onClick={() => dispatch(reset())}>
+              <FontAwesomeIcon icon={faRedo}></FontAwesomeIcon>
+            </Button>
+
+            <Button
+              color="danger"
+              size="lg"
+              onClick={() => dispatch(decrement(1))}
+            >
+              <FontAwesomeIcon icon={faMinus}></FontAwesomeIcon>
+            </Button>
+          </ButtonGroup>
+        </div>
+
+        {isLogged ? <h3>Hello!</h3> : null}
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import counterReducer from "./counter"
+import isLogged from "./isLogged"
+
+import { combineReducers } from 'redux'
+
+const allReducers = combineReducers({
+    counterReducer,
+    isLogged
+})
+
+export default allReducers;
